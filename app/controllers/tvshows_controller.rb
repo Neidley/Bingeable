21bda799781347ca505c5bfd87ae86c1b5c3ab14
Andrew Neidley <neidz44@gmail.com>
2018-09-26 02:03:16 -0500

    search works, still need show

--- app/controllers/tvshows_controller.rb
+++ app/controllers/tvshows_controller.rb
@@ -3,10 +3,17 @@ class TvshowsController < ApplicationController
 
   def index
     response = HTTParty.get("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV["SECRET_API_KEY"]}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false")
-    @top_shows = response["results"] || "\n" + "nothing here, soldier. try again"
+    @top_shows = response["results"]
   end
 
   def show
-    @tvshow = @shows.find(params[:id])
+    @tvshow = @top_shows.find(params[:id])
   end
+
+  def search
+    response = HTTParty.get("https://api.themoviedb.org/3/search/tv?api_key=#{ENV["SECRET_API_KEY"]}&query=#{params[:query]}&page=1")
+    @shows = response["results"]
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'tvshows/show'
 
+  get 'tvshows/search'
+
   root 'tvshows#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
